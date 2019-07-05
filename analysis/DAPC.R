fc1b1f433e7507f3218c9a33fd980610031bd926
homolaj1@msu.edu <jaredhomola20@gmail.com>
2019-07-05 14:14:09 -0400

    Completed DAPC script

--- analysis/DAPC.R
+++ analysis/DAPC.R
@@ -5,23 +5,16 @@
 ##### Load data and libraries #####
 library(adegenet)
 library(vcfR)
-library(egg)
 library(tidyverse)
 library(SeaLampreyRapture)
 
 data(SeaLampreyRapture)
 
-##### Convert data into required formats #####
-dat <- read.vcfR(allLoci) ## Read VCF
-dat.genind <- vcfR2genind(allLoci, sep = "[|/]") ## Create genind object
+##### Convert vcfR data to genind #####
+dat.genind <- vcfR2genind(allLoci.vcfR, sep = "[|/]") ## Create genind object
 
-### Assign pop names to genind file
-## Population-specific
-#popNames <- read.delim("freebayesNames.csv", sep = ",", header = FALSE)
-#dat.genind@pop <- popNames$V1 ## Assign populations to each individual, stored within genind object
-## Age specific
-ageNames <- read.delim("samplenames_age_specific.csv", sep = ",", header = FALSE)
-dat.genind@pop <- ageNames$V1 ## Assign populations to each individual, stored within genind object
+### Assign population and age class names to genind file
+dat.genind@pop <- ageNames$V1
 
 ###### Perform discriminant analysis of principal components ######
 dapc.all <- dapc(dat.genind, var.contrib = TRUE, scale = FALSE, n.pca = 100, n.da = nPop(dat.genind) - 1)
@@ -31,7 +24,6 @@ loadings <- as.data.frame(dapc.all$var.contr)[1:2]
 #loadings$LD1.std <- loadings$LD1/0.00147533
 #loadings$LD2.std <- loadings$LD2/0.0009887752
 
-
 ## Ranking loci by eigenvalue
 perc.rank <- function(x) trunc(rank(x))/length(x)
 loadings.LD1 <- within(loadings, LD1.rank <- perc.rank(LD1))
@@ -39,26 +31,14 @@ loadings.LD1 <- loadings.LD1[order(loadings.LD1$LD1, decreasing = TRUE),]
 loadings.LD1.subset <- subset(loadings.LD1, LD1.rank > 0.9)
 tail(loadings.LD1.subset)
 
-#loadings.LD2 <- within(loadings, LD2.rank <- perc.rank(LD2))
-#loadings.LD2 <- loadings.LD2[order(loadings.LD2$LD2, decreasing = TRUE),]
-#loadings.LD2.subset <- subset(loadings.LD2, LD2.rank > 0.9)
+loadings.LD2 <- within(loadings, LD2.rank <- perc.rank(LD2))
+loadings.LD2 <- loadings.LD2[order(loadings.LD2$LD2, decreasing = TRUE),]
+loadings.LD2.subset <- subset(loadings.LD2, LD2.rank > 0.9)
 #write.csv(loadings, "dapcLoadings.loci.csv")
 
-## Histogram of loadings
-#ggplot(loadings, aes(x=LD1)) +
-#  geom_histogram(binwidth = 0.000015) +
-#  theme_classic(base_size = 18) +
-#  geom_vline(xintercept = 0.0001144738) +
-#  NULL
-#
-#ggplot(loadings, aes(x=LD2)) +
-#  geom_histogram(binwidth = 0.000015) +
-#  theme_classic(base_size = 18) +
-#  geom_vline(xintercept = 0.0001216921) +
-#  NULL
 
-
-### Revised Kim plot - Jan 31, 2019 - Option 1
+### Plot DAPC results
+## Core plot
 dapc.df <- cbind(as.data.frame(dapc.all$ind.coord[,1:4]), dat.genind@pop)
 names(dapc.df) <- c("A1", "A2", "A3", "A4", "pop")
 centroids <- aggregate(cbind(A1, A2)~pop, dapc.df, mean)
@@ -83,8 +63,8 @@ dapc.plot <- ggplot(dapc.df, aes(x = A1, y = A2, color = pop, shape = pop)) +
   NULL
 
 
-### Plot variation explained by retained principal components
-## Set up data frame
+## Plot variation explained by retained principal components
+# Set up data frame
 dapcPCAvar <- dapc.all$pca.eig
 dapcPCAvar <- 100 * cumsum(dapcPCAvar) / sum(dapcPCAvar)
 dapcPCAvar <- as.data.frame(dapcPCAvar)
@@ -95,7 +75,7 @@ dapcPCAvar$color <- cut(
   labels = c("retained", "not retained")
 )
 
-## Perform plotting
+# Perform plotting
 pca.plot <- ggplot(dapcPCAvar, aes(x = comp, y = dapcPCAvar, fill = color)) +
   scale_fill_manual(values = c("gray28", "gray90")) +
   geom_bar(stat = "identity") +
